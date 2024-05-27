ab3639427e1acce9eb099436e479ddacb66639a9
Piatero <fornalepietro@gmail.com>
2024-05-27 10:28:50 +0200

    Added business logic

---
+++ src/main/java/it/intesys/snackbar/snackbar/service/SnackService.java
@@ -0,0 +1,19 @@
+package it.intesys.snackbar.snackbar.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SnackService {
+
+
+    public SnackService() {
+
+    }
+
+    public Boolean orderSnack (String user, String snack) {
+
+
+        return true;
+    }
+
+}
