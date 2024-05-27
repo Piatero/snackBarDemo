ca99ca4d7042122aa16e5b91fb7e2f0d12d83790
Piatero <fornalepietro@gmail.com>
2024-05-27 10:50:27 +0200

    add user existance check

--- src/main/java/it/intesys/snackbar/snackbar/service/SnackService.java
+++ src/main/java/it/intesys/snackbar/snackbar/service/SnackService.java
@@ -1,17 +1,31 @@
 package it.intesys.snackbar.snackbar.service;
 
+import it.intesys.snackbar.snackbar.repository.UserRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SnackService {
 
+    private final UserRepository userRepository;
 
-    public SnackService() {
-
+    public SnackService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public Boolean orderSnack (String user, String snack) {
 
+        if (!userRepository.userExists(user)) {
+            throw new IllegalArgumentException("User " + user + " does not exist");
+        }
+
+
+        // verifico utente ha soldi nel wallet
+        // verifico se esiste lo snack e se è disponibile
+
+        // scalo i soldi dal wallet dell'utente
+        // scalo la disponibilità dello snack
+
+        // verifico se snack cade
 
         return true;
     }
